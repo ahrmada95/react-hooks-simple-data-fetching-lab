3f001e3c9883872fc72aa96a162fd77c26b22bac
Ahnaf Ahmad <ahrmada95@gmail.com>
2022-07-26 11:28:55 -0400

    commit tree fcbf3007b2fdc3127defea184c87ddfeb9979b8f

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
-// create your App component here
+import React, {useState, useEffect} from "react";
+
+const App = () => {
+    const [imgUrl, setImgUrl] = useState('default');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const req = await fetch('https://dog.ceo/api/breeds/image/random')
+            const res = await req.json();
+            console.log(res['message']);
+            setImgUrl(res['message']);
+        }
+        fetchData();
+    }, []);
+
+    if(imgUrl==='default') {
+        return (
+            <p>Loading...</p>
+        )
+    } 
+    
+    return (
+        <img src={imgUrl} alt="A Random Dog"/>
+    )
+
+}
+
+export default App;
